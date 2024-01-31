fb33ddaef767e59bf231f405aad8287a2e984a9f
Deep Gandhi <115660960+DeepGandhi4444@users.noreply.github.com>
2024-01-31 20:22:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
-const fs = require('fs')
+// const fs = require('fs')
 
 const mode = require('./routes/mode.js');
 const api =  require('./routes/api.js');
